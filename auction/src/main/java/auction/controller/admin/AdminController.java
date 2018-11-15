c9e1064b5235550398162f9525e46fd072b72cde
user <user@DESKTOP-7EQF3M8>
2018-11-15 14:29:15 +0900

    온라인경매 경매등록기능 추가중..

--- auction/src/main/java/auction/controller/admin/AdminController.java
+++ auction/src/main/java/auction/controller/admin/AdminController.java
@@ -1,7 +1,12 @@
 package auction.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import auction.entity.Auction;
+
 
 //관리자기능 관련 컨트롤러
 @Controller
@@ -10,6 +15,12 @@ public class AdminController {
 	@RequestMapping("/register")
 	public String register() {
 		return "/admin/register";
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String register(@ModelAttribute Auction auction) {
+		
+		return "redirect:/list";
 	}
 	
 	@RequestMapping("/commission")
