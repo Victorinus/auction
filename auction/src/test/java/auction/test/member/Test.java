a2e7bbb6a32b903ba1033dadfd8c38ec88c07b4c
jinung <creatorjju@gmail.com>
2018-11-29 12:44:31 +0900

    1129 회원작업

--- auction/src/test/java/auction/test/member/Test.java
+++ auction/src/test/java/auction/test/member/Test.java
@@ -1,7 +1,6 @@
 package auction.test.member;
 
 import org.apache.ibatis.session.SqlSession;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,26 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import auction.entity.Member;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"classpath:/auction/application-config.xml"})
-public class JoinTest {
+public class Test {
 	
 	@Autowired
 	private SqlSession sqlsession;
 	
 	private Logger log = LoggerFactory.getLogger(getClass());
 	
-	@Test
+	@org.junit.Test
 	public void test() {
-		log.debug("sqlSession={}", sqlsession);
-		Member member = Member.builder().user_sq(1).user_id("test").user_pw("test").user_nm("test").
-							user_birth("19910601").user_gender("남자").user_phone("123-1234-1234").
-							user_email("test@test.com").user_post("1234").user_addr1("test").user_addr2("test2").
-							reg_dt("20181126").chg_dt("20181126").seesion_key("test").session_limit("1234").build();
+		int totalCount= 25;
+		int countList = 10;
 		
-		sqlsession.insert("regist_user", member);
+		int totalPage = totalCount/countList;
+		
+		if(totalCount%countList>0) {
+			totalPage++;
+		}
 	}
 	
 }
